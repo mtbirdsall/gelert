778c08812dd32941a199f5c941f5321d8d4019f7
Matt Birdsall <m.t.birdsall@gmail.com>
2016-05-08 03:06:29 -0600

    App.py update to include SQL Alchmey connection information.

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/gelert'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(80), unique=True)
+	email = db.Column(db.String(80), unique = True)
+
+	def __init__(self, username, emial):
+		self.username = username
+		self.email = email
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
 
 @app.route('/')
 def index():
